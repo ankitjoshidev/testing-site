813165686986dc5e0382c8dbeb61181d899eac98
Ankit Joshi <ankitjoshi66259@gmail.com>
2023-05-07 21:21:33 +0530

    changes

--- config/database.js
+++ config/database.js
@@ -1,23 +1,9 @@
 const Sequelize = require('sequelize');
 
 const connection = require('./connection');
-
-let database;
-
-// console.info('NODE_ENV:', process.env.NODE_ENV || 'development');
-
-switch ('development') {
-  case 'development':
-    database = new Sequelize({
-      dialect: connection.development.dialect,
-      storage: connection.development.storage,
-    });
-    break;
-  default:
-    database = new Sequelize({
-      dialect: connection.development.dialect,
-      storage: connection.development.storage,
-    });
-}
+const database = new Sequelize({
+  dialect: connection.development.dialect,
+  storage: connection.development.storage,
+});
 
 module.exports = database;
